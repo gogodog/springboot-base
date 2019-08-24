9ac55401a8bed2859925e9b652ec7d340800f2d4
jiadong.wen <ducycat@163.com>
2019-08-25 01:22:10 +0800

    修改版本目錄

---
+++ 2.0.0/src/main/java/com/cott/bootbase/Application.java
@@ -0,0 +1,31 @@
+package com.cott.bootbase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 启动类
+ * @author jiadong.wen
+ *
+ */
+@SpringBootApplication
+@Controller
+public class Application {
+	
+	private static final Logger logs = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		logs.info("启动成功");
+	}
+	
+	@GetMapping("/")
+	public String index(){
+		return "index";
+	}
+
+}
